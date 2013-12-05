cfcc0fb845794b4ac4363074ca8e5b8e465e04fb
Donovan Buck <donovan@donovan.bz>
2013-12-05 18:29:39 -0500

    Add support for serving static files in eg folder

--- demoServer.js
+++ demoServer.js
@@ -1,8 +1,17 @@
 /**
- * The simplest possible server for nodebotui
+ * A simple server for nodebotui
  **/
 
-var app = require('http').createServer(),
+var 
+  url = require('url'),
+  send = require('send'),
+  http = require('http'),
+  
+  app = http.createServer(function(req, res){
+    var root = req.url.indexOf('nodebotui') !== -1 ? 'client/' : 'eg/';
+    send(req, req.url).root(root).pipe(res);
+  }),
+  
   ui = require('./lib/nodebotui').listen(app);
   
 app.listen(3000);
